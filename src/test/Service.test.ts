0b84eb475f3fac73094d8c41341d9f33997b1e22
tphonlabutr <tphonlabutr@hotmail.com>
2025-01-06 01:08:55 +0700

    Fix tests

--- src/test/Service.test.ts
+++ src/test/Service.test.ts
@@ -4,7 +4,7 @@ import { getCoolName } from "../app/Service"
 describe('Service test suite', ()=>{
     test('Name should be cool', ()=>{
         const actual = getCoolName()
-        const expected = 'VeryCoolName';
+        const expected = 'VeryCoolName2';
         expect(actual).toBe(expected)
     })
 })
